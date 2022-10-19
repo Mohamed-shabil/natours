15b0b1f06aed0222d09e0cb9418ffdc4e2dc28bc
Mohamed Shabil <you@example.com>
2022-10-19 14:24:01 +0530

    Error Handling completed

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,18 +1,24 @@
 const AppError = require('./../utils/appError');
+
 const handleCastErrorDB = err =>{
-  console.log('fired');
   const message = `invalid ${err.path}: ${err.value}.`
   return new AppError(message,400)
 }
 
-
 const handleDuplicateFieldsDB = err =>{
-  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0]; 
-  console.log(value)
-  const message = `Duplicate Field value: x. Please use another value`
+  const value = err.keyValue//.match(/(["'])(\\?.)*?\1/);
+   
+  const message = `Duplicate Field value:" ${value.name} "Please use another value`
+  console.log(message);
+  return new AppError(message,400)
+}
 
+const handleValidationErrorDB = err=>{
+  const errors = Object.values(err.errors).map(el => el.message);
+  const message= ` Invalid input data. ${errors.join('. ')}`;
   return new AppError(message,400);
 }
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -24,8 +30,8 @@ const sendErrorDev = (err, res) => {
 
 const sendErrorprod = (err, res) => {
   // Operational, trusted error : send message to client 
-  if (err.isOperational ) {
-    res.status(err, res).json({
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
       status: err.status,
       message: err.message,
     });
@@ -45,13 +51,19 @@ module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
 
-  if (process.env.NODE_ENV === "development") {
+  if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
-  } else if (process.env.NODE_ENV === "production") {
-    let error = {...err};
+  } else if (process.env.NODE_ENV === 'production') {
+
+    let error = { ...err };
+    
+    console.log('its working ',error);
+
     // Cast Error Controller handleCastErrorDB-> Related to DB / handleCastErrorDB is assigned to the middleware err
-    if(error.name === 'CastError') error = handleCastErrorDB(error);
+    if(err.name === 'CastError') error = handleCastErrorDB(error);
     if(error.code === 11000) error = handleDuplicateFieldsDB(error);
-    sendErrorprod(error, res);
+    if(err.name === 'ValidationError') error = handleValidationErrorDB(error);
+    sendErrorprod(error,res);
   }
 };
+
