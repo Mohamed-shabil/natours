2ad6453df1612cc6181ee94e171e68c0d4654191
Mohamed Shabil <mohamedshabil231@gmail.com>
2022-12-01 14:04:53 +0530

    Delete Current User Feature

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,16 @@ const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 const User = require('../models/userModel')
 
+
+const filterObj =(obj, ...allowedFields) =>{
+  const newObj = {};
+
+  Object.keys(obj).forEach(el=>{ 
+    if(allowedFields.includes(el)) newObj[el]= obj[el];
+  }) 
+
+  return newObj;
+}
 exports.getAllUser = catchAsync(async(req, res) => {
   const users = await User.find();
   res.status(200).json({
@@ -14,9 +24,37 @@ exports.getAllUser = catchAsync(async(req, res) => {
   });
 
 
-exports.updateMe = (req,res)=>{
-  
-}
+exports.updateMe = catchAsync(async(req,res, next)=>{
+  // 1) Create Error if user Posts password data
+  if(req.body.password || req.body.passwordConfirm){
+    return next(new AppError('This Route Not  for Password updates. please use /updateMyPassword',400));
+  }
+
+  // 2) filtered out unwanted Fields name that are not allowed to be updated
+  const filteredBody = filterObj(req.body,'name','email');
+
+  // 3 - Update Document
+  const updatedUser = await User.findByIdAndUpdate(req.user.id,filteredBody,{
+    new:true,
+    runValidators:true
+  });
+  res.status(200).json({
+    status: 'Success',
+    data:{
+      user:updatedUser 
+    }
+  })
+})
+
+
+exports.deleteMe = catchAsync(async (req,res,next) => {
+  await User.findByIdAndUpdate(req.user.id,{active:false})
+
+  res.status(204).json({
+    status: "success",
+    data:null
+  })
+})
   
 exports.getUser = (req, res) => {
     res.status(500).json({
--- models/userModel.js
+++ models/userModel.js
@@ -40,7 +40,12 @@ const userSchema= new mongoose.Schema({
     },
     passwordChangedAt: Date,
     passwordResetToken:String,
-    passwordResetExpires:Date
+    passwordResetExpires:Date,
+    active:{
+        type: Boolean, 
+        default:true,
+        select:false
+    }
 })
 
 userSchema.pre('save', async function(next){
@@ -64,6 +69,12 @@ userSchema.pre('save',function(next){
     next();
 })
 
+userSchema.pre(/^find/,function(next){
+    // this point to the current query
+    this.find({ active: { $ne:false} });
+    
+    next(); 
+})
 
 /* 
    -> userSchame.methods. is used to create custom custom fuctions 
